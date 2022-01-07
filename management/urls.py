49c12df05c2e8db5f2d5a3aeed1c5814612a996e
ali2002kh <73595075+ali2002kh@users.noreply.github.com>
2022-01-07 17:33:54 +0330

    debug post edit

--- management/urls.py
+++ management/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.decorators import login_required
 
 
 app_name = 'management'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:pk>/edit', login_required(views.edit.as_view()), name='edit'),
+    path('<int:pk>/edit', views.edit.as_view(), name='edit'),
     path('<int:post_id>/delete', views.delete, name='delete'),
     path('create/', views.create, name='create'),
     path('<int:post_id>/comments/', views.comments, name='comments'),
